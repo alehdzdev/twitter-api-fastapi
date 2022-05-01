f3eff2324fd58174ab99a8262b01ea6256dfa231
Alejandro Hernandez <alehdzdev@gmail.com>
2022-04-30 20:49:07 -0400

    [ADD] model tweet

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
+from signal import default_int_handler
 from uuid import UUID
-from datetime import date
+from datetime import date, datetime
 from typing import Optional
 
 # Pydantic
@@ -12,7 +13,11 @@ class UserBase(BaseModel):
 
 
 class UserLogin(UserBase):
-    password: str = Field(..., min_length=8)
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
 
 
 class User(UserBase):
@@ -30,4 +35,12 @@ class User(UserBase):
 
 
 class Tweet(BaseModel):
-    pass
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=280,
+    )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
